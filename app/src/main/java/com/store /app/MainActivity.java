940277932d2f26f42596a3a0cc698ed48eb2f50d
smart346965-cloud <smart346965@gmail.com>
2026-07-18 01:13:33 +0300

    Change target URL in MainActivity

--- app/src/main/java/com/store /app/MainActivity.java
+++ app/src/main/java/com/store /app/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(activeWebView);
 
         // 3️⃣ توجيه المحرك للهدف
-        String targetUrl = "https://kith.com/"; 
+        String targetUrl = "https://m.shein.com/"; 
         if (activeWebView.getUrl() == null || !activeWebView.getUrl().startsWith("http")) {
             activeWebView.loadUrl(targetUrl);
         }
