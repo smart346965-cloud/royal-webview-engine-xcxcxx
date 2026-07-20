23a11ae043e3663430de1c883be9571822ebc184
smart346965-cloud <smart346965@gmail.com>
2026-07-20 22:23:53 +0300

    Update WebEngineManager.java

--- app/src/main/java/com/store /app/WebEngineManager.java
+++ app/src/main/java/com/store /app/WebEngineManager.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.*;
@@ -529,4 +530,4 @@ public class WebEngineManager {
         }
         return true;
     }
-            }
+                }
